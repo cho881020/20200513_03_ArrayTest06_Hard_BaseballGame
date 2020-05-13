9b7caa7910e6569b444951f0b83554b601947818
weekyeon <weekyeon@gmail.com>
2020-05-13 12:17:16 +0900

    중복 숫자 제거 로직 추가
    - 랜덤 숫자가 배열에 들어있지 않아야 대입
     - 중복 검사를 통과할 때까지 계속 랜덤 숫자를 다시 뽑도록 while(true) 추가

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -9,8 +9,23 @@ public class MainDrive {
 		int[] questionArr = new int[3];
 		
 		for (int i=0 ; i < questionArr.length ; i++) {
-			int randomNum = (int) (Math.random() * 9 + 1);
-			questionArr[i] = randomNum;
+			while (true) {
+				int randomNum = (int) (Math.random() * 9 + 1);
+				
+				boolean isDuplOk = true;
+				for (int num : questionArr) {
+					if (randomNum == num) {
+						isDuplOk = false;
+						break;
+					}
+				}
+				
+				if (isDuplOk) {
+					questionArr[i] = randomNum;
+					break;
+					
+				}
+			}
 		}
 		
 		for (int num : questionArr) {
