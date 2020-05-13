67d3da260069b0aa41a5fc00730511ec2d31affe
toast eggs toast <59465490+sunnysideup0w0v@users.noreply.github.com>
2020-05-13 12:11:44 +0900

    Make random number in Array
    1~9 사이의 randomNumber을 배열에 넣음. 중복숫자 허용되는 상황. 수정필요

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -7,7 +7,9 @@ public class MainDrive {
 		// 야구게임 - 구현정 
 		int[] questionArr = new int[3];
 		for(int i=0;i<questionArr.length;i++) {
-			questionArr[i] = 1;
+			int randomNum = (int)(Math.random()*9+1);
+			questionArr[i] = randomNum;
+			
 		}
 		for(int num : questionArr) {
 			System.out.print(num+" ");
