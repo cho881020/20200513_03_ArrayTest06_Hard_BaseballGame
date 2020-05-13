859a6b298bbd05464da4292e61ccb2084f62b56d
kimhaneui <kimhaneui2@naver.com>
2020-05-13 12:50:55 +0900

    3 스트라이크 할때까지 무한반복
     - 다맞추면 무한반복 종료

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -38,30 +38,39 @@ public class MainDrive {
 		}
 		
 		Scanner scan = new Scanner(System.in);
-		System.out.print("세자리 숫자 입력 : ");
-		int inputNum = scan.nextInt();
 		
-		answer[0] = inputNum/100;
-		answer[1] = (inputNum/10)%10;
-		answer[2] = inputNum%10;
-//		세자리 숫자 배열로 하나씩 받아내기
-		
-		int strike = 0;
-		int ball = 0;
-		
-		for(int i=0;i<answer.length;i++) {
-			for(int j=0;j<question.length;j++) {
-				if(answer[i]==question[j]) {//숫자도 같고
-					if(i==j) { //위치도 같고
-						strike++;
-					}
-					else { //위치는 다름
-						ball++;
+		while(true) {
+			System.out.print("세자리 숫자 입력 : ");
+			int inputNum = scan.nextInt();
+			
+			answer[0] = inputNum/100;
+			answer[1] = (inputNum/10)%10;
+			answer[2] = inputNum%10;
+//			세자리 숫자 배열로 하나씩 받아내기
+			
+			int strike = 0;
+			int ball = 0;
+			
+			for(int i=0;i<answer.length;i++) {
+				for(int j=0;j<question.length;j++) {
+					if(answer[i]==question[j]) {//숫자도 같고
+						if(i==j) { //위치도 같고
+							strike++;
+						}
+						else { //위치는 다름
+							ball++;
+						}
 					}
 				}
 			}
+			System.out.println(strike+"S"+ball+"B");
+			if(strike ==3) {
+				System.out.println("정답입니다");
+				System.out.println("게임을 종료합니다");
+				break; //종료시켜버렷
+			}
 		}
-		System.out.println(strike+"S"+ball+"B");
+		
 	}
 	
 }
