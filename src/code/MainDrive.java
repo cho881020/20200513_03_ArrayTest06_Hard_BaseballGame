4003212e540f93229aec7f1eb48d95b5d3563621
juhyeon2398 <38197106+juhyeon2398@users.noreply.github.com>
2020-05-13 12:06:57 +0900

    Update MainDrive.java

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -4,8 +4,14 @@ public class MainDrive {
 
 	public static void main(String[] args) {
 		
-		// 이름을 남겨주세요.
-		//위
+		
+		int[] questionArr =new int[3];		
+		for(int i=0;i<questionArr.length;i++) {
+			questionArr[i]=(int)(Math.random()*10+1);
+		}
+		for(int num:questionArr) {
+			System.out.println(num);
+		}
 	}
 	
 }
