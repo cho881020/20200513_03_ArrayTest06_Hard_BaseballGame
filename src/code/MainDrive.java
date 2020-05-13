d172aef8f9eca7a0472fe589efc28080b01bc218
juhyeon2398 <38197106+juhyeon2398@users.noreply.github.com>
2020-05-13 12:38:34 +0900

    3자리 입력 처리
    세자리 숫를 입력받아 자리별로 담음

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -1,18 +1,41 @@
 package code;
 
+import java.util.Scanner;
+
 public class MainDrive {
 
 	public static void main(String[] args) {
-		
-		
-		int[] questionArr =new int[3];		
-		for(int i=0;i<questionArr.length;i++) {
-			int randomNum=(int)(Math.random()*9+1);
-			questionArr[i]=randomNum;
+		int[] questionArr = new int[3];
+		int[] answerArr = new int[3];
+		for (int i = 0; i < questionArr.length; i++) {
+			while (true) {
+				int randomNum = (int) (Math.random() * 9 + 1);
+
+				boolean isDuplOk = true;
+				for (int num : questionArr) {
+					if (randomNum == num) {
+						isDuplOk = false;
+						break;
+					}
+				}
+				if (isDuplOk == true) {
+					questionArr[i] = randomNum;
+					break;
+				}
+			}
 		}
-		for(int num:questionArr) {
+		for (int num : questionArr) {
 			System.out.println(num);
 		}
+		Scanner scan = new Scanner(System.in);
+		System.out.print("3자리 정수를 입력하세요 : ");
+		int inputNum = scan.nextInt();
+
+		answerArr[0] = inputNum / 100;
+		answerArr[1] = inputNum / 10 % 10;
+		answerArr[2] = inputNum % 10;
+		
+		
 	}
-	
+
 }
