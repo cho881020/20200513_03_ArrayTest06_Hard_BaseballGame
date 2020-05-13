1a99b17f6cddb47d9e5eaabcfb86bb8a5aac46e3
yhwba <59465345+yhwba@users.noreply.github.com>
2020-05-13 12:08:22 +0900

    문제용 세자리 배열 생성
    - 일단 세자리 모두 i 로 대입

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -4,9 +4,16 @@ public class MainDrive {
 
 	public static void main(String[] args) {
 		
-		// 이름을 남겨주세요.
+		// 야구 게임 : 유혜원
 		
+		int[] questionArr = new int[3];
+		 for (int i = 0; i< questionArr.length; i++) {
+			 questionArr[i] =1;
+		 }
 		
+		 for ( int num :questionArr ) {
+			 System.out.println(num);
+		 }
 	}
 	
 }
