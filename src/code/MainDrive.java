da5c28fe6a79e402fe00086818a09918493da3cf
kimhaneui <kimhaneui2@naver.com>
2020-05-13 12:38:15 +0900

    사용자 숫자입력처리
     - 세자리 숫자 입력
     - 각자리를 따서 배열에 담아둔다

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -1,5 +1,8 @@
 package code;
 
+import java.util.Scanner;
+
+
 public class MainDrive {
 
 	public static void main(String[] args) {
@@ -7,18 +10,41 @@ public class MainDrive {
 		// 김하늬
 		
 		int[] question = new int[3];
+		int[] answer = new int[3];
+		
 		
 		for(int i=0;i<question.length;i++) {
+			while(true) {
+				int random = (int) (Math.random()*9+1);
+				
+				boolean isOk = true;
+				//중복 피해야져
+				for(int num : question) {
+					if(random == num) {
+						isOk = false;
+						break;
+					}
+				}
+				if(isOk) {
+					question[i] = random;
+					break; //다음칸에 넣을 숫자를 구하기위해 나감
+				}
+			}
 			
-			int random = (int) (Math.random()*9+1);
-			
-			question[i] = random;
 		}
 		
 		for(int num : question) {
 			System.out.println(num);
 		}
 		
+		Scanner scan = new Scanner(System.in);
+		System.out.print("세자리 숫자 입력 : ");
+		int inputNum = scan.nextInt();
+		
+		answer[0] = inputNum/100;
+		answer[1] = (inputNum/10)%10;
+		answer[2] = inputNum%10;
+//		세자리 숫자 배열로 하나씩 받아내기
 	}
 	
 }
