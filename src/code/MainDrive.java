4fcf5c495f41041032ee835e3b98ddee04e7da11
HwangboSeok <59465263+hbs9312@users.noreply.github.com>
2020-05-13 12:06:48 +0900

    문제용 배열 세자리 생성
    - 세자리 모두 1로 출력
    - 세자리를 랜덤한 숫자로 출력되게 변경 예정

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -4,8 +4,16 @@ public class MainDrive {
 
 	public static void main(String[] args) {
 		
-		// 야구게임 - 황보석
+		// 야구게임 - 황보석]
 		
+		int[] questionArr = new int[3];
+		
+		for(int i=0; i < questionArr.length; i++) {
+			questionArr[i] = 1;
+		}
+		for(int num : questionArr) {
+			System.out.println(num);
+		}
 	}
 	
 }
