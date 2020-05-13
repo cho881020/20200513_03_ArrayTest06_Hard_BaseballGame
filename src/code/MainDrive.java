614fce1c02e609b0995d867dd8148d3d550d1ceb
yhwba <59465345+yhwba@users.noreply.github.com>
2020-05-13 12:38:51 +0900

    사용자 숫자 입력처리
    - 세자리 숫자 입력 받아두자
    - 각자리를 따서 배열에 담아둔다.

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -1,36 +1,53 @@
 package code;
 
+import java.util.Scanner;
+
 public class MainDrive {
 
 	public static void main(String[] args) {
-		
+
 		// 야구 게임 : 유혜원
-		
+
 		int[] questionArr = new int[3];
-		 for (int i = 0; i< questionArr.length; i++) {
-//			 0<=(int) (Math.random()*9+1)<10	//랜덤넘버남겨두기	
-			 
-			 int randomNum = (int) (Math.random()*9+1);
-			 
-			 boolean isDuplOk = true;
-			 
-			 for( int num : questionArr) {
-				 if(randomNum == num) {
-					 isDuplOk = false;
-					 break;
-				 }
-			if(isDuplOk) {
-				questionArr[i] =randomNum;
-				break;
-			}
-			 }
-			 
-			 
-		 }
+		int[] answerArr = new int[3];
 		
-		 for ( int num :questionArr ) {
-			 System.out.println(num);
-		 }
+		
+		for (int i = 0; i < questionArr.length; i++) {
+//			 1<=(int) (Math.random()*9+1)<10	//랜덤넘버남겨두기	
+			while(true) {
+				
+				int randomNum = (int) (Math.random() * 9 + 1);
+				
+				boolean isDuplOk = true;
+				
+				for (int num : questionArr) {
+					if (randomNum == num) {
+						isDuplOk = false;
+						break;
+					}
+					
+				}
+				
+				if (isDuplOk) {
+					questionArr[i] = randomNum;
+					break;
+				}
+			}
+
+		}
+
+		for (int num : questionArr) {
+			System.out.println(num);
+		}
+		Scanner scan = new Scanner(System.in);
+		System.out.print("세자리 숫자를 입력 : ");
+		int inputNum = scan.nextInt();
+		
+		answerArr[0] = inputNum / 100;
+		answerArr[1] = (inputNum / 10) % 10; // 가운데 자리수를 얻으려면 192 => 10으로 나누고 19.2 => 나머지 를 구한다.
+		answerArr[2] = inputNum % 10;
+		
+		
 	}
-	
+
 }
