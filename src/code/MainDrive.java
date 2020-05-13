15fa49b854b66e58710b12fb8d89ea9cb6d2d0d9
HwangboSeok <59465263+hbs9312@users.noreply.github.com>
2020-05-13 12:53:20 +0900

    Update MainDrive.java

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -37,12 +37,45 @@ public class MainDrive {
 		int[] answerArr = new int[3];
 		
 		Scanner scan = new Scanner(System.in);
-		System.out.println("세자리 정수 숫자를 입력 : ");
-		int inputNum = scan.nextInt();
+		int inputCount=0;
 		
-		answerArr[0] = inputNum / 100;
-		answerArr[1] = inputNum %10 %10;
-		answerArr[2] = inputNum % 10;
+		while(true) {
+			System.out.println("세자리 정수 숫자를 입력 : ");
+			int inputNum = scan.nextInt();
+			
+			inputCount++;
+			
+			answerArr[0] = inputNum / 100;
+			answerArr[1] = inputNum /10 %10;
+			answerArr[2] = inputNum % 10;
+			int strikeCount = 0;
+			int ballCount = 0;
+			
+			for(int i=0; i < answerArr.length; i++) {
+				
+				for(int j=0; i < questionArr.length; j++) {
+					
+					if(answerArr[i] == questionArr[j]) {
+						if(i == j) {
+							strikeCount++;
+						}
+						else {
+							ballCount++;
+						}
+					}
+				}
+				System.out.println(strikeCount + "S " + ballCount+ "B 입니다.");
+				
+				if(strikeCount == 3) {
+					System.out.println("정답입니다.");
+					System.out.println(inputCount + "회만에 맞췄습니다.");
+					System.out.println("게임을 종료합니다.");
+					break;
+				}
+			}
+		}
+		
+		
 		
 	}
 	
