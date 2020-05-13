ee2a2fe8bc4c21ac597f36040fcefaec51c3a294
toast eggs toast <59465490+sunnysideup0w0v@users.noreply.github.com>
2020-05-13 12:06:29 +0900

    Update MainDrive.java
    Arr

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -4,8 +4,14 @@ public class MainDrive {
 
 	public static void main(String[] args) {
 		
-		// 야구게임 - 구현정 음냠
-		
+		// 야구게임 - 구현정 
+		int[] questionArr = new int[3];
+		for(int i=0;i<questionArr.length;i++) {
+			questionArr[i] = 1;
+		}
+		for(int num : questionArr) {
+			System.out.print(num+" ");
+		}
 	}
 	
 }
