69eb1c1cf707fc0d5da80f82734346ed855374c4
dy696786 <dy696786@gmail.com>
2020-05-13 12:07:38 +0900

    배열 추가

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -6,6 +6,16 @@ public class MainDrive {
 		
 		// 야구게임 - 이다연
 		
+		int[] questionArr = new int[3];
+		
+		for(int i=0; i < questionArr.length; i++) {
+			questionArr[i] = 1;
+		}
+		
+		for(int num:questionArr) {
+			System.out.println(num);
+		}
+		
 	}
 	
 }
