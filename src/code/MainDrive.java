c6dff33277627c1aa26a494725f596c2757fe73e
toast eggs toast <59465490+sunnysideup0w0v@users.noreply.github.com>
2020-05-13 12:19:50 +0900

    Revert "Certificate the numbers' duplication"
    This reverts commit 3e0e4e79f0fb9831585b57b0bb193df1ccc9fe0c.

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -7,20 +7,9 @@ public class MainDrive {
 		// 야구게임 - 구현정 
 		int[] questionArr = new int[3];
 		for(int i=0;i<questionArr.length;i++) {
-			while(true) {				
-				int randomNum = (int)(Math.random()*9+1);
-				boolean isDuplOk = true;
-				for(int num : questionArr) {
-					if(randomNum == num) {
-						isDuplOk = false;
-						break;
-					}
-				}
-				if(isDuplOk) {					
-					questionArr[i] = randomNum;
-					break;
-				}
-			}
+			int randomNum = (int)(Math.random()*9+1);
+			questionArr[i] = randomNum;
+			
 		}
 		for(int num : questionArr) {
 			System.out.print(num+" ");
