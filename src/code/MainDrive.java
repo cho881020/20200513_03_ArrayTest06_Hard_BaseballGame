8ecff510788b5a20caa3bbdbdbd14d992a991e49
weekyeon <weekyeon@gmail.com>
2020-05-13 12:06:11 +0900

    문제용 세자리 배열 생성
     - 일단 세자리 모두 1로 대입
     - 랜덤 값으로 변경할 예정

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -6,6 +6,16 @@ public class MainDrive {
 		
 		// 김정민
 		
+		int[] questionArr = new int[3];
+		
+		for (int i=0 ; i < questionArr.length ; i++) {
+			questionArr[i] = 1;
+		}
+		
+		for (int num : questionArr) {
+			System.out.println(num);
+		}
+		
 	}
 	
 }
