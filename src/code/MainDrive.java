52a52b9130b3751cc25bb026df67e3cba4311ea7
juhyeon2398 <38197106+juhyeon2398@users.noreply.github.com>
2020-05-13 12:11:22 +0900

    1~9사이 랜덤값 대입

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -7,7 +7,8 @@ public class MainDrive {
 		
 		int[] questionArr =new int[3];		
 		for(int i=0;i<questionArr.length;i++) {
-			questionArr[i]=(int)(Math.random()*10+1);
+			int randomNum=(int)(Math.random()*9+1);
+			questionArr[i]=randomNum;
 		}
 		for(int num:questionArr) {
 			System.out.println(num);
