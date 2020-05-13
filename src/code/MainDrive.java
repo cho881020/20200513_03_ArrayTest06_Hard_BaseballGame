3e0e4e79f0fb9831585b57b0bb193df1ccc9fe0c
toast eggs toast <59465490+sunnysideup0w0v@users.noreply.github.com>
2020-05-13 12:17:42 +0900

    Certificate the numbers' duplication
    중복되어있지 않으면 > 입력
    중복되어있으면 > 중복되지 않을 때까지  계속해서 randomNumber 뽑기.

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -7,9 +7,20 @@ public class MainDrive {
 		// 야구게임 - 구현정 
 		int[] questionArr = new int[3];
 		for(int i=0;i<questionArr.length;i++) {
-			int randomNum = (int)(Math.random()*9+1);
-			questionArr[i] = randomNum;
-			
+			while(true) {				
+				int randomNum = (int)(Math.random()*9+1);
+				boolean isDuplOk = true;
+				for(int num : questionArr) {
+					if(randomNum == num) {
+						isDuplOk = false;
+						break;
+					}
+				}
+				if(isDuplOk) {					
+					questionArr[i] = randomNum;
+					break;
+				}
+			}
 		}
 		for(int num : questionArr) {
 			System.out.print(num+" ");
